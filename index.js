169c31a53e71bee0edb445d563064bf5a9ff244d
vegito11 <omkardbhujbal@gmail.com>
2023-03-20 01:52:22 +0530

    :hammer: updated app 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-	res.send('Hi There I am testng ')
+	res.send('Hi There I am testing 1 ')
 })
 
 app.listen(8080,()=>{
