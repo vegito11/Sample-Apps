acdb89df68eafbb40b4b355a97ab8a66fb8de5f5
vegito11 <omkardbhujbal@gmail.com>
2023-11-03 17:02:27 +0530

    :construction_worker: added add and mul method

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require('express')
+const os = require('os');
+
 
 const app = express()
 
+const version = "V1.0.1"
+
 app.get('/',(req,res) => {
-	res.send('Hi There I am testing 3453245r4 ')
+	resp = 'Hi welcome to Sample node App : ' + version
+	res.send(resp)
 })
 
+app.get('/add',(req,res) => {
+	
+	offset = 10000 
+	num1 = Number.parseInt(req.query.num1 || 0)
+	num2 = Number.parseInt(req.query.num2 || 0)
+	result = num1 + num2 + offset
+	res.send(` addition of ${num1} + ${num2} with offset of ${offset} : ${result}`)
+})
+
+app.get('/mul',(req,res) => {
+	
+	num1 = Number.parseInt(req.query.num1 || 1)
+	num2 = Number.parseInt(req.query.num2 || 1)
+	result = num1 * num2
+	res.send(` Multiplication of ${num1} * ${num2} with offset of: ${result}`)
+})
+
+app.get('/whoami', (req, res) => {
+  
+  const ipAddresses = Object.fromEntries(Object.entries(os.networkInterfaces()).map(([key, val]) => [key, val.find(entry => entry.family === 'IPv4').address]));
+
+  const userInfo = {
+    user: os.userInfo().username,
+    hostname: os.hostname(),
+    platform: os.platform(),
+    arch: os.arch(),
+    Ips: ipAddresses
+  };
+
+  res.json(userInfo);
+});
 app.listen(8080,()=>{
 	console.log("Hi I am Listening on port 8080");
 })
