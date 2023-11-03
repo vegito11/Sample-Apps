55923bea4aca73a6625d3ae4952c96d0c3a0c939
vegito11 <omkardbhujbal@gmail.com>
2023-11-03 17:50:42 +0530

    :construction_worker: added app2 calling support

--- controllers/test_controller.py
+++ controllers/test_controller.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, jsonify, request
 import os
+import requests
+import traceback
+
 
 test_controller = Blueprint('test_controller', __name__)
 
@@ -14,3 +17,26 @@ def test():
         "username": USERNAME,
         "env_name": ENVIRONMENT
     })
+
+@test_controller.route('/app2_call/<path:api_endpoint>', methods=['GET'])
+@test_controller.route('/app2_call/', methods=['GET'])
+def get_data(api_endpoint=""):
+    try:
+        
+        api_host = os.getenv("APP2_URL", "http://localhost:8080/")
+        api_url = f'{api_host}{api_endpoint}'
+        
+        print(api_url)
+        query_params = request.args
+
+        response = requests.get(api_url, params=query_params)
+        
+        if response.status_code == 200:
+            data = response.json()
+            return jsonify(data)
+        else:
+            return jsonify({"error": "Failed to retrieve data from the external API"}), 500
+    
+    except Exception as e:
+        # traceback.print_exc()
+        return jsonify({"error": str(e)}), 500    
